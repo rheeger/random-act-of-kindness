add8cfa1bb1284aaf3b6722b01bdc65ffebd83e3
Robbie Heeger <rheeger@gmail.com>
2024-01-29 01:06:39 -0500

    rename reskin

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -23,7 +23,7 @@ OrgTitle=${encodeURIComponent(org.name)}&
 OrgDescription=${encodeURIComponent(String(org.description))}&
 ${org.logoUrl && `OrgLogo=${encodeURIComponent(String(org.logoUrl))}&`}
 ${org.address && `OrgLocation=${encodeURIComponent(org.address.city + ', ' + org.address.state)}&`}
-NTEETitle=${encodeURIComponent(org.nteeDescription)}&
+NTEETitle=${encodeURIComponent(String(org.nteeDescription))}&
 NTEEIcon=${encodeURIComponent(`https://app.endaoment.org/images/ntee/v2/@256w/${orgMajorCode}.png`)}`;
 
     console.log(getOrgMetaTagImageURL)
@@ -32,7 +32,7 @@ NTEEIcon=${encodeURIComponent(`https://app.endaoment.org/images/ntee/v2/@256w/${
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="${getOrgMetaTagImageURL}" />
     <meta property="fc:frame:button:1" content="Search Again" />
-    <meta property="fc:frame:post_url" content="https://random-act-of-kindness.vercel.app/api/frame" />
+    <meta property="fc:frame:post_url" content="https://random-frame-of-kindness.vercel.app/api/frame" />
     <meta property="fc:frame:button:2" content="Donate .001 ETH" />
     <meta property="fc:frame:button:2:action" content="redirect" />
   </head></html>`);
--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,16 @@ import fetchRandomOrg from "./random-org";
 const frameMetadata = getFrameMetadata({
   buttons: ['Find a Nonprofit'],
   image: 'https://storage.googleapis.com/endaoment-static/endaoment-meta-preview.png',
-  post_url: 'https://random-act-of-kindness.vercel.app/api/frame',
+  post_url: 'https://random-frame-of-kindness.vercel.app/api/frame',
 });
 
 export const metadata: Metadata = {
-  title: 'Random Act of Kindness | Frame Generator',
+  title: 'Random Frame of Kindness | Frame Generator',
   description: 'Give it away, give it away, give it away now.',
   openGraph: {
-    title: 'Random Act of Kindness | Frame Generator',
-    description: 'Random Act of Kindness | Frame Generator',
-    images: ['https://storage.googleapis.com/endaoment-static/endaoment-meta-preview.png'],
+    title: 'Random Frame of Kindness | Frame Generator',
+    description: 'Random Frame of Kindness | Frame Generator',
+    images: ['https://storage.googleapis.com/endaoment-static/random-frame-of-kindness.png'],
   },
   other: {
     ...frameMetadata,
@@ -27,8 +27,6 @@ export default function Page() {
         <h1><a href='https://app.endaoment.org/explore'>The meaning of life is to find your gift. The purpose of life is
           to give it away.</a></h1>
         <h2>— Pablo Picasso</h2>
-        <h3>Give it away, give it away, give it away now.</h3>
-        <h4>— Red Hot Chili Peppers</h4>
       </>
   );
 }
